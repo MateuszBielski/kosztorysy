3252cb04e6ba04b0b6bfe15814965999ab472885
Mateusz Bielski <mateo.bass@interia.pl>
2020-03-09 15:04:28 +0100

    ReplaceCharsAccordingUtf8 working

--- src/Service/Functions.php
+++ src/Service/Functions.php
@@ -9,4 +9,63 @@ class Functions
     public static function Hello(){
         return "Hello";
     }
+    public function LoopOverMBlistEncodings(Type $var = null)
+    {
+        setlocale(LC_ALL, 'pl_PL');
+        $charset = array_slice(mb_list_encodings(),1);
+        foreach ($charset as $key => $cSet) {
+            try{
+                $converted_str = iconv($cSet,'UTF-8',$str_bytes);
+            }catch(Exception $e){
+                continue;
+            }
+            if ($converted_str == 'śćłóężą')
+            print_r($key.$cSet);
+            break;
+        }
+        
+        var_dump($converted_str);
+    }
+    
+    public static function ReplaceCharsAccordingUtf8(string $str_bytes)
+    {
+        $str_result = "";
+        $rep = null;
+        $str_numbers = unpack('C*',$str_bytes);
+        foreach ($str_numbers as $n_char) 
+        {
+            switch ($n_char){
+                case 158://ś
+                $rep = pack('C2',197,155);
+                break;
+                case 141://ć
+                $rep = pack('C2',196,135);
+                break;
+                case 146://ł
+                $rep = pack('C2',197,130);
+                break;
+                case 162://ó
+                $rep = pack('C2',195,179);
+                break;
+                case 145://ę
+                $rep = pack('C2',196,153);
+                break;
+                case 167://ż
+                $rep = pack('C2',197,188);
+                break;
+                case 134://ą
+                $rep = pack('C2',196,133);
+                break;
+                default:
+                $rep = pack('C',$n_char);
+
+            }
+            $str_result .= $rep;
+        }
+
+        return $str_result;
+    }
+    //inna wersja
+    // https://kasztelan.me/mazovia-utf-8-php-function/
+    
 }
--- tests/ReplacePolishEncodedCharTest.php
+++ tests/ReplacePolishEncodedCharTest.php
@@ -18,6 +18,7 @@ class ReplacePolishEncodedCharTest extends TestCase
     private $path_file2 = __DIR__.'/../resources/Norma3/Kat/0-22/0-22R5.OP';
     private $path_file3 = __DIR__.'/../resources/litery.txt';
     private $path_file4 = __DIR__.'/../resources/polskieZnaki.txt';
+    private $path_file5 = __DIR__.'/../resources/kodowanieUTF8.txt';
     public function testSomething()
     {
         $this->assertTrue(true);
@@ -41,27 +42,42 @@ class ReplacePolishEncodedCharTest extends TestCase
         $this->assertEquals('abcdef 12345',$raw_data);
         fclose($f);
     }
-    public function testReplacCharsAccordingUtf8()
+    public function testReplaceCharsAccordingUtf8()
     {
         // $f = fopen($this->path_file4,'rb');
         // $byteCharStreamToReplace = fread($f,filesize($this->path_file4));
         // Functions::CharsAccordingUtf8()
         // print_r(mb_list_encodings());
         $str_bytes = pack('C*',158,141,146,162,145,167,134);
-        setlocale(LC_ALL, 'pl_PL');
-        $charset = array_slice(mb_list_encodings(),1);
-        foreach ($charset as $key => $cSet) {
-            try{
-                $converted_str = iconv($cSet,'UTF-8',$str_bytes);
-            }catch(Exception $e){
-                continue;
-            }
-            if ($converted_str == 'śćłóężą')
-            print_r($key.$cSet);
-            break;
-        }
-        
-        var_dump($converted_str);
+        $replaced_chars = Functions::ReplaceCharsAccordingUtf8($str_bytes);
+        $u_data = unpack('C*',$replaced_chars);
+        $str_numbers = implode(" ",$u_data);
+        $this->assertEquals('197 155 196 135 197 130 195 179 196 153 197 188 196 133',$str_numbers);
+
+    }
+
+    public function testUnpackigReadStream()
+    {
+        $f = fopen($this->path_file5,'rb');
+        $raw_data = fread($f,filesize($this->path_file5));
+        $u_data = unpack('C*',$raw_data);
+        $str_data = implode(" ",$u_data);
+        $this->assertEquals('97 98 99 100 101 102 10 197 155 196 135 197 130 195 179 196 153 197 188 196 133',$str_data);
+        fclose($f);
+    }
+
+    public function testReadSiglePolishCharPacked()
+    {
+        $polichChar = pack('C2',197,155);
+        $this->assertEquals('ś',$polichChar);
+    }
+    public function testReadAndConvertNormaO(Type $var = null)
+    {
+        $f = fopen($this->path_file2,'rb');
+        $raw_data = fread($f,filesize($this->path_file2));
+        // $u_data = unpack('C*',$raw_data);
+        $convertedString = Functions::ReplaceCharsAccordingUtf8($raw_data);
+        echo $convertedString;
 
     }
 }
